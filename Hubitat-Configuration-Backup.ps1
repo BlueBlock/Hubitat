28a0432fe0ccb50cc76c6911cedf2c7bca76381a
BlueBlock <theblueblock@protonmail.com>
2020-02-28 14:53:34 -0500

    fix $url

--- Hubitat-Configuration-Backup.ps1
+++ Hubitat-Configuration-Backup.ps1
@@ -1,6 +1,6 @@
 # Name: Hubitat-Configuration-Backup.ps1
 # Purpose: To backup latest hubitat configuration to off-device location
-# version: 1.0.0
+# Version: 1.0.1
 
 # ex: .\Hubitat-Configuration-Backup.ps1 -HubHostname 192.168.1.99 -BackupDestination c:\temp -WhatIf
 # ex: .\Hubitat-Configuration-Backup.ps1 -HubHostname 192.168.1.99 -BackupDestination c:\temp
@@ -28,18 +28,18 @@ $output = $web.DownloadString($urlhub)
 [System.Net.ServicePointManager]::ServerCertificateValidationCallback = $null
 
 # hub location/name
-$WebResponseObj = Invoke-WebRequest -Uri "$urlhub/location/edit" -TimeoutSec 15 -ErrorAction:Stop
+$WebResponseObj = Invoke-WebRequest -Uri "$urlhub/location/edit"
 $location = ($WebResponseObj.Forms | Where {$_.Action -eq "/location/update"}).Fields.name
 
 # hub software version
-$WebResponseObj = Invoke-WebRequest -Uri "$urlhub/hub/edit" -TimeoutSec 15 -ErrorAction:Stop
+$WebResponseObj = Invoke-WebRequest -Uri "$urlhub/hub/edit"
 $version = $WebResponseObj.ParsedHtml.IHTMLDocument3_getElementByID("hubPopup").getElementsByClassName("menu-text")[0].outerText.Trim()
 
 # get and save backup file
-$url = "http://hubitat/hub/backupDB?fileName=latest"
+$url = "$($urlhub)/hub/backupDB?fileName=latest"
 $output = "$BackupDestination\$($BackupNamePrefix)$($location)_$(get-date -f yyyy-MM-dd-HHmmss)_$($version).lzf"
 if ($WhatIf) {
     write-host "WhatIf: Would write backup to $output" -ForegroundColor Cyan
 } else {
-    Invoke-WebRequest -OutFile $output -Uri $url -TimeoutSec 15 -ErrorAction:Stop
+    Invoke-WebRequest -OutFile $output -Uri $url
 }
